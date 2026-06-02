0fc5d0fb41099e5051b17d99335a5fbec50ca4bf
parkjoohyeong06 <parkjoohyeong@naver.com>
2026-06-02 13:48:54 +0900

    Create Player class with name and color attributes

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player {
+
+    private String name;
+    private int color;
+
+    public Player(String name, int color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
